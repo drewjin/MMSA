ca1a759e94656ee35d37a12af02dc82481d5d36a
Drew <11528145+jyj2431567@user.noreply.gitee.com>
2024-06-26 14:03:11 +0800

    Problem Solved

--- src/MMSA/data_loader.py
+++ src/MMSA/data_loader.py
@@ -162,7 +162,7 @@ class MMDataset(Dataset):
         self.text = do_truncate(self.text, text_length)
         self.audio = do_truncate(self.audio, audio_length)
 
-    def __normalize(self): # TODO: why mfn utilizes this function?
+    def __normalize(self): 
         # (num_examples,max_len,feature_dim) -> (max_len, num_examples, feature_dim)
         self.vision = np.transpose(self.vision, (1, 0, 2))
         self.audio = np.transpose(self.audio, (1, 0, 2))
